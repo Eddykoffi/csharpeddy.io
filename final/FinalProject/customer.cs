d5736e49683c1d1b5a22e070b71d508a751e9775
edmundkwakye <tallboyeddy@gmail.com>
2023-07-13 23:14:25 +0100

    customer.cs

---
+++ final/FinalProject/customer.cs
@@ -0,0 +1,17 @@
+public class Customer
+{
+    protected string _name;
+
+    public string Name { get; set; }
+
+    public void InteractWithPet(Pet pet)
+    {
+        // Implementation for customer's interaction with a pet
+    }
+
+    public void TakePetToVeterinary(Pet pet, Veterinary vet)
+    {
+        // Implementation for customer taking a pet to the veterinary clinic
+    }
+}
+
