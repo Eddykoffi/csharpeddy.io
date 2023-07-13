4e4a89046506c41484a4eb98ffd0521c121dd239
edmundkwakye <tallboyeddy@gmail.com>
2023-07-13 21:05:20 +0100

    vet.cs

---
+++ final/FinalProject/vet.cs
@@ -0,0 +1,17 @@
+public class Veterinary
+{
+    public void Checkup(Pet pet)
+    {
+        // Implementation for performing a checkup on a pet
+    }
+
+    public void Vaccinate(Pet pet)
+    {
+        // Implementation for administering vaccinations to a pet
+    }
+
+    public void TreatInjury(Pet pet)
+    {
+        // Implementation for treating a pet's injury or ailment
+    }
+}
