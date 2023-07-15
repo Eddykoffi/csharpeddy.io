56b1e57fccecc5113c8a00242f88f8408ec1747a
edmundkwakye <tallboyeddy@gmail.com>
2023-07-15 21:09:23 +0100

    Update pet.cs

--- final/FinalProject/pet.cs
+++ final/FinalProject/pet.cs
@@ -1,23 +1,12 @@
-public class Pet
+public abstract class Pet : IPet
 {
-    protected string name;
-    protected int age;
+    protected string _name;
+    protected int _age;
 
     public string Name { get; set; }
     public int Age { get; set; }
 
-    public virtual void Eat()
-    {
-        // Implementation for eating behavior
-    }
-
-    public virtual void Play()
-    {
-        // Implementation for playing behavior
-    }
-
-    public virtual void Sleep()
-    {
-        // Implementation for sleeping behavior
-    }
+    public abstract void Eat();
+    public abstract void Play();
+    public abstract void Sleep();
 }
