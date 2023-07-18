14b8ca99e4735b384208d9a99b1cfdfa104980a4
edmundkwakye <tallboyeddy@gmail.com>
2023-07-18 23:40:37 +0100

    Create EternalGoal.cs

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,16 @@
+class EternalGoal : Goal
+{
+    public EternalGoal(string name, int value) : base(name, value)
+    {
+    }
+
+    public override void CompleteGoal()
+    {
+        Console.WriteLine("Cannot complete an Eternal Goal.");
+    }
+
+    public override void RecordEvent()
+    {
+        Console.WriteLine($"Recorded event for Eternal Goal: {Name}");
+    }
+}
